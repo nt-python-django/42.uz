28bf7931231173dfa26067d394cb99ff3e2083b7
djumanov <djumanovdev@gmail.com>
2026-03-14 10:28:30 +0500

    bot login

--- api/bot.py
+++ api/bot.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from telegram import Bot, Update
 from telegram.ext import Dispatcher, CommandHandler
 
-from .handlers import start
+from .handlers import start, login
 
 
 bot = Bot(settings.TG_BOT_TOKEN)
 dispatcher = Dispatcher(bot, None, workers=4, use_context=True)
 
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('login', login))
 
 
 def handle_update(data: dict):
--- api/handlers.py
+++ api/handlers.py
@@ -1,6 +1,47 @@
+from random import randint
+
 from telegram import Update
 from telegram.ext import CallbackContext
 
+from .models import User
+
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Hello! I'm your bot. How can I assist you today?")
+    chat_id = update.effective_chat.id
+    user_id = update.effective_user.id
+    first_name = update.effective_chat.first_name
+    last_name = update.effective_chat.last_name
+
+    existing_user = User.objects.filter(username=user_id).first()
+    if existing_user:
+        welcome_message = f"Welcome back, {first_name}!"
+        context.bot.send_message(chat_id=chat_id, text=welcome_message)
+        return
+    # Save user info to the database
+    user = User.objects.create(
+        username=user_id,
+        chat_id=chat_id,
+        first_name=first_name,
+        last_name=last_name
+    )
+    welcome_message = f"Hello, {first_name}! You have been registered successfully."
+
+    context.bot.send_message(chat_id=chat_id, text=welcome_message)
+
+
+def login(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    user_id = update.effective_user.id
+    first_name = update.effective_chat.first_name
+    last_name = update.effective_chat.last_name
+
+    existing_user = User.objects.filter(username=user_id).first()
+    if not existing_user:
+        welcome_message = f"Hello, {first_name}! You are not registered yet. Please use /start to register."
+        context.bot.send_message(chat_id=chat_id, text=welcome_message)
+        return
+    
+    opt = randint(100000, 999999)
+
+    update.message.reply_text(f"Your OTP is: {opt}")
+    
